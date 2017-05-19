4c73636373bf06c254a3fd085bbc4a6e582d036a
william <1257344995@qq.com>
2017-05-19 16:05:22 +0800

    司机表添加车辆号码

--- src/main/java/com/lhcx/service/impl/OrderServiceImpl.java
+++ src/main/java/com/lhcx/service/impl/OrderServiceImpl.java
@@ -13,6 +13,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.lhcx.dao.OrderMapper;
 import com.lhcx.model.Order;
 import com.lhcx.model.OrderType;
+import com.lhcx.service.IDriverLocationService;
 import com.lhcx.service.IOrderService;
 import com.lhcx.utils.MD5Kit;
 import com.lhcx.utils.Utils;
@@ -23,6 +24,8 @@ public class OrderServiceImpl implements IOrderService {
 
 	@Autowired
 	private OrderMapper orderMapper;
+	@Autowired
+	private IDriverLocationService driverLocationService;
 	
 	public int insertSelective(Order order) {
 		return orderMapper.insertSelective(order);
@@ -55,28 +58,20 @@ public class OrderServiceImpl implements IOrderService {
 	public Map<String,Object> match(JSONObject jsonRequest) throws ParseException {
 		Map<String,Object> result = new HashMap<String, Object>();
 		String orderId = jsonRequest.getString("OrderId");
-		String longitude = jsonRequest.getString("Longitude");
-		String latitude = jsonRequest.getString("Latitude");
-		Integer encrypt = jsonRequest.getInteger("Encrypt");
-		String licenseId = jsonRequest.getString("LicenseId"); 
+
 		String driverPhone = jsonRequest.getString("DriverPhone");
-		String vehicleNo = jsonRequest.getString("VehicleNo");
-		Date distributeTime = Utils.toDateTime(jsonRequest.getLong("DistributeTime"));
+
+		Date distributeTime = new Date();
 		
 		Order order = selectByOrderId(orderId);
 		order.setDriverphone(driverPhone);
-		order.setLongitude(longitude);
-		order.setLatitude(latitude);
-		order.setEncrypt(encrypt);
-		order.setLicenseid(licenseId);
-		order.setVehicleno(vehicleNo);
 		order.setDistributetime(distributeTime);
 		order.setStatus(OrderType.Receiving.value());
 		updateByOrderIdSelective(order);
 		
 		result.put("OrderId", orderId);
 		result.put("DriverPhone", driverPhone);
-		result.put("VehicleNo", vehicleNo);
+		result.put("VehicleNo", order.getVehicleno());
 		result.put("DistributeTime", distributeTime);
 		
 		return result;
