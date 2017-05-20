59adecc01c39640cce7fe59cdc046b2a19fb8882
AnYangWuYanZu <842154692@qq.com>
2017-05-20 14:06:19 +0800

    极光推送appkey

--- src/main/java/com/lhcx/utils/ConfigUtils.java
+++ src/main/java/com/lhcx/utils/ConfigUtils.java
@@ -10,11 +10,11 @@ public class ConfigUtils {
 	//推送距离
 	public static final double PUSH_DISTANCE = 50000;
 	
-	//极光AppKey a1835850adc6bff63a9bf7c6
-	public static final String JPUSH_APP_KEY = "f2f3161d4c78b07151dc3df0";
+	//极光AppKey 
+	public static final String JPUSH_APP_KEY = "a1835850adc6bff63a9bf7c6";
 	
-	//极光秘钥  68f13ab0dfd2e888d11522a0
-	public static final String JPUSH_MASTER_SECRET = "daf0aa323b562a5afdf197b9";
+	//极光秘钥  
+	public static final String JPUSH_MASTER_SECRET = "68f13ab0dfd2e888d11522a0";
 	
 	//乘客端极光AppKey
 	public static final String PASSENGER_JPUSH_APP_KEY = "5cb88b6bcdd00a001d6df99f";
